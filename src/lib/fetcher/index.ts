96a1742e8f1e35d34d15f71dd7a3a6b32fc0d283
maitake4211 <dmwajtgp4211@gmail.com>
2023-03-06 18:48:26 +0900

    dbから値を取得して表示できた

--- src/lib/fetcher/index.ts
+++ src/lib/fetcher/index.ts
@@ -3,6 +3,6 @@ import {Fetcher} from 'swr';
 
 // SWRで使うfetcher
 // asyncでラップしているからPromiseを返す
-const fetcher: Fetcher<URL, string> = url => axios.get(url).then(res => res.data);
+const fetcher = (url: string) => axios.get(url).then(res => res.data);
 
 export default fetcher;
