ea629069007a35b23e57d8c2dc29d92838b68532
maitake4211 <dmwajtgp4211@gmail.com>
2023-03-06 19:04:57 +0900

    id追加

--- src/components/DeviceList/index.tsx
+++ src/components/DeviceList/index.tsx
@@ -14,7 +14,7 @@ const DeviceList: React.FC  = () =>{
   if (!devices) return <div>Now Loading...</div>;
   return (
     <>
-    <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+    <ul id="deviceSelectRadio" className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
       {devices.map((device)=>(
         <li key={`li-${device.id}`} className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
           <input key={`input-${device.id}`} id={device.location} type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
