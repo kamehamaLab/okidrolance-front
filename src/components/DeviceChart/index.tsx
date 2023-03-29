2ccd3695c5772ee2b8e4db26fb046a28a3122328
maitake4211 <dmwajtgp4211@gmail.com>
2023-03-29 13:21:23 +0900

    温度以外も表示

--- src/components/DeviceChart/index.tsx
+++ src/components/DeviceChart/index.tsx
@@ -37,6 +37,8 @@ const DeviceChart: React.FC<{data: MonitorData}> = ({data}) => {
       type: 'datetime',
     },
     yaxis: {
+      decimalsInFloat: 0,
+      tickAmount:8,
       min: 0,
       max: 80
     }
@@ -70,6 +72,7 @@ const DeviceChart: React.FC<{data: MonitorData}> = ({data}) => {
       }
     },
     yaxis: {
+      decimalsInFloat: 0,
       tickAmount: 2,
       min: 0,
       max: 80
@@ -77,7 +80,9 @@ const DeviceChart: React.FC<{data: MonitorData}> = ({data}) => {
   };
 
   return(
-    <div id="tempWrapper">
+    <>
+    <div id="tempWrapper" className="w-4/5 mx-auto my-10 border-4 p-5">
+      <div className="text-center text-xl">温度</div>
       <div id="chart">
         <Chart options={optionMainLine} series={[{name: "temp", data: data.temp}]} type="line" height={230} />
       </div>
@@ -85,6 +90,25 @@ const DeviceChart: React.FC<{data: MonitorData}> = ({data}) => {
         <Chart options={optionSubLine} series={[{name: "temp", data: data.temp}]} type="area" height={130} />
       </div>
     </div>
+    <div id="w_tempWrapper" className="w-4/5 m-auto my-10 border-4 p-5">
+      <div className="text-center text-xl">水温</div>
+      <div id="chart">
+        <Chart options={optionMainLine} series={[{name: "w_temp", data: data.w_temp}]} type="line" height={230} />
+      </div>
+      <div id="subChart">
+        <Chart options={optionSubLine} series={[{name: "w_temp", data: data.w_temp}]} type="area" height={130} />
+      </div>
+    </div>
+    <div id="iliimWrapper" className="w-4/5 m-auto my-10 border-4 p-5">
+      <div className="text-center text-xl">照度</div>
+      <div id="chart">
+        <Chart options={optionMainLine} series={[{name: "illum", data: data.illum}]} type="line" height={230} />
+      </div>
+      <div id="subChart">
+        <Chart options={optionSubLine} series={[{name: "illum", data: data.illum}]} type="area" height={130} />
+      </div>
+    </div>
+    </>
   );
 };
 
